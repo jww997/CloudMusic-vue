ed125ee1a4f18fcef1db4f0f128bf4292e32da53
jww997 <1551005987@qq.com>
2020-04-17 17:26:12 +0800

    村乐

--- src/pages/Home/Discover/discover.vue
+++ src/pages/Home/Discover/discover.vue
@@ -1,85 +1,38 @@
 <template>
   <!-- 发现 -->
-  <view class="discover">
-
+  <div class="discover">
     <discover-banner :banners="discover.banners" />
     <discover-menus :menus="discover.menus" />
     <discover-recommends :recommends="discover.recommends" />
-123
-  </view>
+  </div>
 </template>
 
 <script>
+import DiscoverBanner from "./components/banner"; // 轮播图
+import DiscoverMenus from "./components/menus"; // 菜单
+import DiscoverRecommends from "./components/recommends"; // 热门推荐
 
-  import DiscoverBanner from './components/banner'; // 轮播图
-  import DiscoverMenus from './components/menus'; // 菜单
-  import DiscoverRecommends from './components/recommends'; // 热门推荐
-
-  export default {
-    name: "discover",
-    components: {
-      DiscoverBanner,
-      DiscoverMenus,
-      DiscoverRecommends,
-    },
-    data() {
-      const that = this;
-      return {
-        discover: {
-          banners: [], // 轮播图
-          menus: [{
-            name: '每日推荐',
-            iconClass: 'icon-dailySpecial',
-            dataTo: '',
-          }, {
-            name: '歌单',
-            iconClass: 'icon-songList',
-            dataTo: 'songListSquare',
-          }, {
-            name: '排行榜',
-            iconClass: 'icon-rankingList',
-            dataTo: 'rankingList',
-          }, {
-            name: '电台',
-            iconClass: 'icon-radioStation',
-            dataTo: 'radioStation',
-          }, {
-            name: '私人FM',
-            iconClass: 'icon-FM',
-            dataTo: '',
-          }], // 菜单栏
-          recommends: [], // 推荐歌单
-        },
-      }
-    },
-    computed: {
-      swiper() {
-        return this.$refs.mySwiper.swiper;
-      }
-    },
-    methods: {
-      getdata(api, callBack) {
-        axios.get('http://localhost:3000/' + api)
-          .then(res => {
-            if (res.status == 200) {
-              callBack(res);
-            };
-          })
-      },
-    },
-    mounted() {
-      const that = this;
-      that.getdata('banner', res => {
-        that.discover.banners = res.data.banners;
-      });
-      that.getdata('personalized', res => {
-        console.log(res.data.result);
-        that.discover.recommends = res.data.result;
-      });
+export default {
+  name: "discover",
+  props: {
+    discover: {
+      type: Object,
+      value: {}
     }
+  },
+  components: {
+    DiscoverBanner,
+    DiscoverMenus,
+    DiscoverRecommends
+  },
+  data() {
+    return {};
   }
+};
 </script>
 
 <style scoped>
-
+.discover {
+  height: 100%;
+}
 </style>
--- src/pages/Home/home.vue
+++ src/pages/Home/home.vue
@@ -4,18 +4,28 @@
     <div class="topBar">
       <div class="iconfont icon-sidebar"></div>
       <div class="topBar-pagination">
-        <div class="swiper-slide-visible topBar-pagination-item"
-          :class="homeSwiperIndex == index? 'topBar-pagination-item-active': ''" v-for="(item,index) in homeSwiperItems"
-          :key="index" @click="toggleHomeSwiperIndex" :data-id="index">{{item}}</div>
+        <div
+          class="swiper-slide-visible topBar-pagination-item"
+          :class="homeSwiperIndex == index? 'topBar-pagination-item-active': ''"
+          v-for="(item,index) in homeSwiperItems"
+          :key="index"
+          @click="toggleHomeSwiperIndex"
+          :data-id="index"
+        >{{item}}</div>
       </div>
       <div class="iconfont icon-search"></div>
     </div>
-    <swiper class="homeSwiper" :options="homeSwiperOption" :activeIndex="homeSwiperIndex" ref="mySwiper">
+    <swiper
+      class="homeSwiper"
+      :options="homeSwiperOption"
+      :activeIndex="homeSwiperIndex"
+      ref="mySwiper"
+    >
       <!-- 我的 -->
       <swiper-slide>我的（待开发）</swiper-slide>
       <!-- 发现 -->
       <swiper-slide>
-        <discover />
+        <discover :discover="discover" />
       </swiper-slide>
       <!-- 云村 -->
       <swiper-slide>云村（待开发）</swiper-slide>
@@ -26,100 +36,139 @@
 </template>
 
 <script>
-  import axios from "axios";
-  import Discover from './Discover/discover'; // 发现
+import axios from "axios";
+import Discover from "./Discover/discover"; // 发现
 
-  export default {
-    name: "Home",
-    components: {
-      Discover,
-    },
-    data() {
-      const that = this;
-      return {
-        homeSwiperItems: ["我的", "发现", "云村", "视频"],
-        homeSwiper: {},
-        homeSwiperIndex: 1, // 容器下标
-        homeSwiperOption: {
-          initialSlide: 1, // 初始下标
-          watchSlidesProgress: true,
-          watchSlidesVisibility: true,
-          on: {
-            slideChange: function (event) {
-              that.homeSwiper = this;
-              that.homeSwiperIndex = this.activeIndex;
-            }
+export default {
+  name: "Home",
+  components: {
+    Discover
+  },
+  data() {
+    const that = this;
+    return {
+      homeSwiperItems: ["我的", "发现", "云村", "视频"],
+      homeSwiper: {},
+      homeSwiperIndex: 1, // 容器下标
+      homeSwiperOption: {
+        initialSlide: 1, // 初始下标
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
+        on: {
+          slideChange: function(event) {
+            that.homeSwiper = this;
+            that.homeSwiperIndex = this.activeIndex;
           }
         }
-      };
-    },
-    computed: {
-      swiper() {
-        return this.$refs.mySwiper.swiper;
-      }
-    },
-    methods: {
-      toggleHomeSwiperIndex(event) {
-        // 容器下标
-        console.log("event.target.dataset.id=", event.target.dataset.id);
-        console.log(this.homeSwiper.activeIndex);
       },
-      getdata(api, callBack) {
-        axios.get("http://localhost:3000/" + api).then(res => {
-          if (res.status == 200) {
-            callBack(res);
+      discover: {
+        banners: [], // 轮播图
+        menus: [
+          {
+            name: "每日推荐",
+            iconClass: "icon-dailySpecial",
+            dataTo: ""
+          },
+          {
+            name: "歌单",
+            iconClass: "icon-songList",
+            dataTo: "songListSquare"
+          },
+          {
+            name: "排行榜",
+            iconClass: "icon-rankingList",
+            dataTo: "rankingList"
+          },
+          {
+            name: "电台",
+            iconClass: "icon-radioStation",
+            dataTo: "radioStation"
+          },
+          {
+            name: "私人FM",
+            iconClass: "icon-FM",
+            dataTo: ""
           }
-        });
+        ], // 菜单栏
+        recommends: [] // 推荐歌单
       }
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
+  methods: {
+    toggleHomeSwiperIndex(event) {
+      // 容器下标
+      console.log("event.target.dataset.id=", event.target.dataset.id);
+      console.log(this.homeSwiper.activeIndex);
     },
-    mounted() { }
-  };
+    getdata(api, callBack) {
+      axios.get("http://localhost:3000/" + api).then(res => {
+        if (res.status == 200) {
+          callBack(res);
+        }
+      });
+    }
+  },
+  mounted() {
+    const that = this;
+    that.getdata("banner", res => {
+      that.discover.banners = res.data.banners;
+    });
+    that.getdata("personalized", res => {
+      that.discover.recommends = res.data.result;
+    });
+  }
+};
 </script>
 
 <style scoped>
-  .container {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-  }
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
 
-  .topBar {
-    flex-shrink: 0;
-    height: 0.8rem;
-    line-height: 0.8rem;
-    text-align: center;
-    background-color: #f00;
-    color: #fff;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+.topBar {
+  flex-shrink: 0;
+  height: 0.8rem;
+  line-height: 0.8rem;
+  text-align: center;
+  background-color: #f00;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .topBar>.iconfont {
-    padding: 0 0.3rem;
-  }
+.topBar > .iconfont {
+  padding: 0 0.3rem;
+}
 
-  .topBar-pagination {
-    font-size: 0.25rem;
-    color: rgba(255, 255, 255, 0.5);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+.topBar-pagination {
+  font-size: 0.25rem;
+  color: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .topBar-pagination-item {
-    width: 1.2rem;
-  }
+.topBar-pagination-item {
+  width: 1.2rem;
+}
 
-  .topBar-pagination-item-active {
-    font-size: 0.3rem;
-    font-weight: bold;
-    color: #fff;
-  }
+.topBar-pagination-item-active {
+  font-size: 0.3rem;
+  font-weight: bold;
+  color: #fff;
+}
 
-  .homeSwiper {
-    width: 100%;
-    flex-grow: 1;
-  }
+.homeSwiper {
+  width: 100%;
+  flex-grow: 1;
+}
 </style>

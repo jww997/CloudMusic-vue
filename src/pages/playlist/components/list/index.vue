6d2a1f17a5b1c06f4e9c3e7cf292f42dfb02aa87
jww997 <1551005987@qq.com>
2020-12-02 16:51:05 +0800

    新增歌单详情列表

--- src/pages/playlist/components/list/index.vue
+++ src/pages/playlist/components/list/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="container">123</div>
+  <div class="container">
+    <div>播放全部</div>
+    <div>
+      <div v-for="(item, index) in obj.tracks" :key="index">
+        {{ item.name }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "List",
+  props: {
+    obj: {
+      type: Object,
+      value: {},
+    },
+  },
 };
 </script>
 

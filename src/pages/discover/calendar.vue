842de64ff0f46732cf0330dd9ea763ce60fa4dfa
jww997 <1551005987@qq.com>
2021-06-05 11:18:19 +0800

    feat:新增音乐日历

--- src/pages/discover/calendar.vue
+++ src/pages/discover/calendar.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="children">
-    <block :block="block">音乐日历</block>
+  <div class="calendar">
+    <block :block="block">
+      <div class="resources">
+        <div class="resource" v-for="item in list" :key="item.id">
+          <div class="left">
+            {{ item.resources[0].uiElement.mainTitle.title }}
+          </div>
+          <div class="right">
+            <img :src="item.resources[0].uiElement.image.imageUrl" />
+          </div>
+        </div>
+      </div>
+    </block>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
-import Block from "./components/block.vue"
+import Block from "./components/block.vue";
 export default {
   name: "calendar",
   props: ["block"],
@@ -29,4 +40,26 @@ export default {
 <style lang="scss" scoped>
 @import "~sass/mixins.scss";
 @import "~sass/varibles.scss";
+.calendar {
+  .resource {
+    height: 2.5rem;
+    padding: 0 .3rem;
+    box-sizing: border-box;
+    border-top: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left {
+      font-size: 0.3rem;
+    }
+    .right {
+      width: 1.5rem;
+
+      img {
+        width: 100%;
+        border-radius: .2rem;
+      }
+    }
+  }
+}
 </style>
